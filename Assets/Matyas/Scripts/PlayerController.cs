0b97051aafa295091d88b82cbdc8468f6875346a
Matyas <matyasgyimesi@gmail.com>
2025-01-24 23:30:22 -0500

    Added PlayerController2 to have better turning.

--- Assets/Matyas/Scripts/PlayerController.cs
+++ Assets/Matyas/Scripts/PlayerController.cs
@@ -5,11 +5,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float movespeed = 1f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float drag = 0.1f;
+    [SerializeField] private float turnSpeed = 200f;
 
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Vector2 velocity;
 
     private void Awake()
     {
@@ -32,17 +35,36 @@ public class PlayerController : MonoBehaviour
         PlayerInput();
     }
 
-    private void FixedUpdate(){
+    private void FixedUpdate()
+    {
         Move();
     }
 
     private void PlayerInput()
     {
-        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        movement += playerControls.Movement.Move.ReadValue<Vector2>();
     }
 
     private void Move()
     {
-        rb.MovePosition(rb.position + movement * (movespeed * Time.fixedDeltaTime));
+        // Calculate the desired velocity based on input
+        Vector2 desiredVelocity = movement * moveSpeed;
+
+        // Interpolate between current velocity and desired velocity to create momentum
+        velocity = Vector2.Lerp(velocity, desiredVelocity, Time.fixedDeltaTime);
+
+        // Apply drag to simulate friction
+        velocity *= (1 - drag * Time.fixedDeltaTime);
+
+        // Move the object based on velocity
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+        // Calculate rotation based on the velocity direction, making sure it's smooth
+        if (velocity != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg - 90f;
+            float angle = Mathf.LerpAngle(rb.rotation, targetAngle, turnSpeed * Time.fixedDeltaTime);
+            rb.rotation = angle;
+        }
     }
 }
---
+++ Assets/Matyas/Scripts/PlayerController2.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+public float MaxSpeed;
+public float acceleration;
+public float steering;
+
+public float drift = 2.0f;
+
+Rigidbody2D rb;
+
+float x;
+float y = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        Vector2 speed = transform.up * (y * acceleration);
+        rb.AddForce(speed);
+
+        float direction = Vector2.Dot(rb.linearVelocity, rb.GetRelativeVector(Vector2.up));
+
+        if(acceleration > 0)
+        {
+            if(direction > 0)
+            {
+                rb.rotation -= x * steering * (rb.linearVelocity.magnitude / MaxSpeed);
+            }
+            else
+            {
+                rb.rotation += x * steering * (rb.linearVelocity.magnitude / MaxSpeed);
+            }
+        }
+
+        float driftForce = Vector2.Dot(rb.linearVelocity, rb.GetRelativeVector(Vector2.left)) * drift;
+
+        Vector2 relativeForce = Vector2.right * driftForce;
+
+        rb.AddForce(rb.GetRelativeVector(relativeForce));
+
+        if(rb.linearVelocity.magnitude > MaxSpeed)
+        {
+            rb.linearVelocity = rb. linearVelocity.normalized * MaxSpeed;
+        }
+
+        //Debug.Drawline(rb.position, rb.GetRelatvePoint(relativeForce), Color.green);
+    }
+}
