48364c371135779dedf49552494fc2cedad64661
Jacob Locker <jacoblocker@Jacobs-MBP.attlocal.net>
2025-01-25 17:50:09 -0500

    Add player health field/event hookup

--- Assets/Main/Scripts/Player/PlayerController.cs
+++ Assets/Main/Scripts/Player/PlayerController.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
+    public UnityEvent<int> healthChangeEvent;
+
 public float MaxSpeed;
 public float acceleration;
 public float steering;
 
 public float drift = 2.0f;
+
+[SerializeField]
+private int health = 5;
+
+[SerializeField]
+private int maxHealth = 10;
 
 Rigidbody2D rb;
 
@@ -17,6 +26,9 @@ float y = 1;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        if (healthChangeEvent == null) {
+            healthChangeEvent = new UnityEvent<int>();
+        }
     }
 
     // Update is called once per frame
@@ -57,5 +69,26 @@ float y = 1;
         }
 
         //Debug.DrawLine(rb.position, rb.GetRelativePoint(relativeForce), Color.green, 2, false);
+    }
+
+    public void Damage(int amount) {
+        SetHealth(health - amount);
+    }
+
+    public void Heal(int amount) {
+        SetHealth(health + amount);
+    }
+
+    public int GetHealth() {
+        return health;
+    }
+
+    private void SetHealth(int newHealth) {
+        health = newHealth;
+        healthChangeEvent.Invoke(health);
+    }
+
+    public int GetMaxHealth() {
+        return maxHealth;
     }
 }
