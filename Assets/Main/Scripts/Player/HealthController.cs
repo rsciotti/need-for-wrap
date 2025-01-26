68fbe3c14162ddda2c4476ff0c79e4e3132f1703
Jacob Locker <jacoblocker@Jacobs-MBP.attlocal.net>
2025-01-25 20:39:19 -0500

    Update with master

--- Assets/Main/Scripts/Player/HealthController.cs
+++ Assets/Main/Scripts/Player/HealthController.cs
@@ -11,6 +11,18 @@ public class HealthController : MonoBehaviour
     [SerializeField]
     private int maxHealth = 10;
 
+    [SerializeField]
+    private Vector2 healthBarOffset;
+
+    [SerializeField]
+    private float healthBarHorizontalSpacing;
+
+    [SerializeField]
+    private Sprite healthBarSprite;
+
+    [SerializeField]
+    private Sprite missingHealthSprite;
+
     private GameObject[] healthBubbleObjs;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,8 +38,14 @@ public class HealthController : MonoBehaviour
 
     private void CreateHealthBar() {
         Vector2 position = gameObject.transform.position;
-        foreach (GameObject healthBubble in healthBubbleObjs) {
+        for (int i = 0; i < maxHealth; i++) {
+            healthBubbleObjs[i] = new GameObject();
+            healthBubbleObjs[i].name = "HealthGameObject(" + i + ")";
+            healthBubbleObjs[i].transform.parent = gameObject.transform;
+            healthBubbleObjs[i].transform.position = new Vector2(healthBarOffset.x + position.x + i * healthBarHorizontalSpacing, healthBarOffset.y + position.y);
             
+            SpriteRenderer renderer = healthBubbleObjs[i].AddComponent<SpriteRenderer>();
+            renderer.sprite = healthBarSprite;
         }
     }
 
